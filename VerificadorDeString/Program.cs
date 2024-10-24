d6606a4951dfc4270fa5f2471716532614ce9513
Arilson <juniorarilson870@gmail.com>
2024-10-24 12:04:11 -0300

    Conta as letras 'a' da string

---
+++ VerificadorDeString/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificadorDeString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool sair = false;
+
+            do
+            {
+                Console.Write("Escreva uma frase OU texto: ");
+                string entrada = Console.ReadLine();
+
+                int contagem = ContarOcorrenciasA(entrada);
+
+                bool existe = contagem > 0;
+
+                if (existe)
+                {
+                    Console.WriteLine($"\nA letra 'a' (maiúscula ou minúscula) aparece {contagem} vez(es) na string.\n");
+                }
+                else
+                {
+                    Console.WriteLine("\nA letra 'a' (maiúscula ou minúscula) não aparece na string.\n");
+                }
+
+                Console.WriteLine("Deseja fazer outra consulta?\n" +
+                   "[1] - Sim\n" +
+                   "[2] - Não");
+
+                int escolha = Convert.ToInt32(Console.ReadLine());
+
+                if (escolha == 2)
+                {
+                    sair = true;
+                }
+
+            } while (!sair);
+        }
+
+        static int ContarOcorrenciasA(string str)
+        {
+            int contagem = 0;
+
+            foreach (char c in str)
+            {
+                if (c == 'a' || c == 'A')
+                {
+                    contagem++;
+                }
+            }
+            return contagem;
+        }
+    }
+}
